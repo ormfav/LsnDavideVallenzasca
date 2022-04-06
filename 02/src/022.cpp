2dc347fa9dd2920831074cf558ee30fbd993e703
TheElegantTurtle <darvide1998@gmail.com>
2022-04-06 22:44:18 +0200

    Completed excercise

--- 02/src/022.cpp
+++ 02/src/022.cpp
@@ -13,12 +13,21 @@ int Sgn(double x){
     return (x>=0)-(x<0);
 }
 
-point<int,3> Rw_discrete(Random &rnd, point<int,3>p, int lattice_dim){
-    int direction=rnd.Rannyu(0,3); 
+void RwDiscrete(point<int,3>& p, Random &rnd, int lattice_dim){
+    int dir=rnd.Rannyu(0,3); 
     int verse=Sgn(rnd.Rannyu(-1,1)); 
-        p.GetPoint()[direction]+=verse*lattice_dim;
-    return p;
+    p[dir]+=verse*lattice_dim;
 };
+
+void RwContinuous(point<double,3>& p, Random &rnd, int lattice_dim){
+    double theta=acos(1-rnd.Rannyu(0,2));
+    double phi=rnd.Rannyu(0,2*M_PI); 
+
+    p[0]+=lattice_dim*sin(theta)*cos(phi);
+    p[1]+=lattice_dim*sin(theta)*sin(phi);
+    p[2]+=lattice_dim*cos(theta);
+};
+
 
 int main (int argc, char *argv[])
 {
@@ -29,35 +38,64 @@ int main (int argc, char *argv[])
 
     Random rnd("lib/Random/Primes","lib/Random/seed.in");
 
+
+    /* Idea:
+     * vettore di punti ciascuno evolverà indipendentemente con rws
+     * evolvo tutti i random walk di un passo e salvo il risultato
+     * in un vettore di datablocks. Evolvo tutti di un altro passo
+     * e salvo la distanza in un vettore di datablocks
+     * -> ottengo così una misura in datablocsk della distanza ad
+     *  ogni passo*/
+
+
+
     //caso discreto
-    point<int,3> rws[RW_PER_BLOCK*N_BLOCKS]; //numero totale di rw
-    vector<dataBlocks> discrete;
-    discrete.reserve(RW_STEPS);
+    point<int,3> discrete_rw[RW_PER_BLOCK*N_BLOCKS]; //numero totale di rw
+    vector<dataBlocks> discrete_dist;
+    discrete_dist.reserve(RW_STEPS);
     for (int i=0; i<RW_STEPS; ++i){
         int k=-1;
         dataBlocks t(N_BLOCKS,RW_PER_BLOCK,[&](){
             k++;
-            rws[k].Evolve(bind(Rw_discrete,rnd,placeholders::_1,LATTICE_DIM));
-            return rws[k].Lenght();
-            });
-        discrete.push_back(t);
+            RwDiscrete(discrete_rw[k],rnd,LATTICE_DIM);
+            return discrete_rw[k].Lenght();
+        });
+        discrete_dist.push_back(t);
     }
 
+    ofstream fout("02/Data/022-discreterw.csv");
+    fout<<"average,variance\n";
+    for (dataBlocks x : discrete_dist ){
+        double ave,var;
+        x.Stats(ave,var);
+        fout<<ave<<","<<var<<endl;
+    }
+    fout.close();
 
 
-    ////caso continuo
-    //for (int i=1; i<N_STEPS; ++i) {
-    //    double theta=rnd.Rannyu(0,M_PI); 
-    //    double phi=rnd.Rannyu(0,2*M_PI); 
+    //caso continuo
+    point<double,3> continuous_rw[RW_PER_BLOCK*N_BLOCKS]; //numero totale di rw
+    vector<dataBlocks> continuous_dist;
+    continuous_dist.reserve(RW_STEPS);
+    for (int i=0; i<RW_STEPS; ++i){
+        int k=-1;
+        dataBlocks t(N_BLOCKS,RW_PER_BLOCK,[&](){
+            k++;
+            RwContinuous(continuous_rw[k],rnd,LATTICE_DIM);
+            return continuous_rw[k].Lenght();
+        });
+        continuous_dist.push_back(t);
+    }
 
-    //    coord_continue[i][0]+=coord_continue[i-1][0]+LATTICE_DIM*sin(theta)*cos(phi);
-    //    coord_continue[i][1]+=coord_continue[i-1][1]+LATTICE_DIM*sin(theta)*cos(phi);
-    //    coord_continue[i][2]+=coord_continue[i-1][2]+LATTICE_DIM*cos(theta);
-    //}
-    //
+    fout.open("02/Data/022-continuousrw.csv");
+    fout<<"average,variance\n";
+    for (dataBlocks x : continuous_dist ){
+        double ave,var;
+        x.Stats(ave,var);
+        fout<<ave<<","<<var<<endl;
+    }
+    fout.close();
+
     
-
-
-
     return 0;
 }
